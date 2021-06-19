3b98913a86f869e20d3df0287a2505f1b5be6edd
Dan <bmdmartinez@gmail.com>
2021-06-19 12:33:19 -0400

    added all questions in an object array

---
+++ assets/javascript/script.js
@@ -0,0 +1,43 @@
+var quizQuestions = [{
+        question: "Functions are used to _______",
+        answerA: "Convert strings into numbers",
+        answerB: "Display an alert message",
+        answerC: "Perform an action",
+        answerD: "Style elements",
+        correctAnswer1: "c"
+    },
+    {
+        question: "CSS stands for:",
+        answerA: "Computer System Storage",
+        answerB: "Cascading Style Sheets",
+        answerC: "Competitive Style Sheets",
+        answerD: "Cascading Short Styles",
+        correctAnswer2: "b"
+    },
+    {
+        question: "Which of the following is NOT a falsey value?",
+        answerA: "false",
+        answerB: "null",
+        answerC: "0",
+        answerD: "'false'",
+        correctAnswer3: "d"
+    },
+    {
+        question: "The text run between the parenthesis () of a function is called a ___",
+        answerA: "Method",
+        answerB: "Object",
+        answerC: "Variable",
+        answerD: "Parameter",
+        correctAnswer4: "d"
+    },
+    {
+        question: "The platform commonly used to host repositories and websites is",
+        answerA: "Github",
+        answerB: "Stack Overflow",
+        answerC: "Bing",
+        answerD: "Wordpress",
+        correctAnswer5: "a"
+    }
+];
+
+console.log(quizQuestions.correctAnswer1);    
