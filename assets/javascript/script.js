5b097613c720c1549b62fda20061320a0c194ea0
Dan <bmdmartinez@gmail.com>
2021-06-19 14:13:30 -0400

    got first question and answers to generate when starting quiz

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,15 @@
+// DOM variables
+
+var startButton = document.getElementById("btn-start");
+var timer = document.getElementById("countdown");
+var startQuizPage = document.getElementById("quiz-start");
+var button1 = document.getElementById("button-A");
+var button2 = document.getElementById("button-B");
+var button3 = document.getElementById("button-C");
+var button4 = document.getElementById("button-D");
+var questionText = document.getElementById("question-text");
+
+// Question array
 var quizQuestions = [{
         question: "Functions are used to _______",
         answerA: "Convert strings into numbers",
@@ -40,4 +52,24 @@ var quizQuestions = [{
     }
 ];
 
-console.log(quizQuestions.correctAnswer1);    
+// variables after object array so they can take the value of the object
+var finalQuestion = quizQuestions.length;
+var currentQuestion = 0;
+
+
+var generateQuestions = function() {
+    // if (currentQuestion === finalQuestion);
+    // alert("Game Over!");
+    var activeQuestion = quizQuestions[currentQuestion];
+    questionText.innerHTML = "<p>" + activeQuestion.question + "<p>";
+    button1.innerHTML = activeQuestion.answerA;
+    button2.innerHTML = activeQuestion.answerB;
+    button3.innerHTML = activeQuestion.answerC;
+    button4.innerHTML = activeQuestion.answerD;
+};
+var startQuiz = function() {
+    // startQuizPage.style.display = "none";
+    generateQuestions();
+}
+
+startButton.addEventListener("click", startQuiz); 
